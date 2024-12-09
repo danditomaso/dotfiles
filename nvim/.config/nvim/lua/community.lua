17e3db101571ded2fdf3ca9e0477a45121946b2b
Dan Ditomaso <dan.ditomaso@gmail.com>
2024-12-09 10:40:49 -0500

    feat: tweaking nvim config

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -4,15 +4,14 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-  -- { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.mdx" },
-  { import = "astrocommunity.pack.sql" },
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.utility.hover-nvim" },
-  { import = "astrocommunity.scrolling.mini-animate" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.typescript-all-in-one" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.mdx" },
+	{ import = "astrocommunity.pack.sql" },
+	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+	{ import = "astrocommunity.utility.hover-nvim" },
+	{ import = "astrocommunity.scrolling.mini-animate" },
+	-- import/override with your plugins folder
 }
---
+++ nvim/.config/nvim/lua/plugins/package-info.lua
@@ -0,0 +1,8 @@
+return {
+	-- other plugin configurations...
+
+	{
+		"vuki656/package-info.nvim",
+		enabled = false, -- This disables the plugin entirely
+	},
+}
